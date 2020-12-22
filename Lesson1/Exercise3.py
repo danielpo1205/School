c5b4ebe90c84fc97acd60235d790b82cbee902e3
Daniel <danielmp1@gmail.com>
2020-12-22 15:50:45 +0200

    Lists

---
+++ Lesson1/Exercise3.py
@@ -0,0 +1,13 @@
+#Lists
+
+new_list=[2,6.6,"daniel",[]]
+print(type(new_list))
+print(new_list)
+
+new_list2=new_list+[77, "Hello"]
+print(new_list2)
+
+new_list3=(new_list2*2)+new_list+["big"]
+print(new_list3)
+
+
