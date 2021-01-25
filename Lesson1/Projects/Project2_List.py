60ed690f6c9ecc00b9e63d8d389a68e4f77ebce9
Daniel <danielmp1@gmail.com>
2021-01-25 12:25:41 +0200

    List

---
+++ Lesson1/Projects/Project2_List.py
@@ -0,0 +1,4 @@
+# Placing the names of 4 colors in a list, then printing the first and last colors on the screen
+
+color_list = ["Red","Green","White","Black"]
+print(color_list[0], ",", color_list[3])
