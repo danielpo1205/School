493eedf389e0d26b3a20c599ed968d133e38d19c
Daniel <danielmp1@gmail.com>
2021-01-25 11:55:42 +0200

    File handling in a code: lists, appending, creating a new file, etc

--- Lesson1/Exercise_File_hand2.py
+++ Lesson1/Exercise_File_hand2.py
@@ -18,12 +18,25 @@
 # file.close()
 # print(my_list)
 
-# Another option for adding multiple lines in a file to a list and then printing it to the screen - line by line
+# # Another option for adding multiple lines in a file to a list and then printing it to the screen - line by line
+# filename = "C:/Users/danie/Documents/סייבר ותכנות/Python/File_handling.txt"
+# file = open(filename, "r")
+# new_list = (file.read().splitlines())
+# print(type(new_list))
+# print(new_list)
+# file.close()
+# for i in new_list:
+#     print(i)
+
+# Printing specific lines from a file
 filename = "C:/Users/danie/Documents/סייבר ותכנות/Python/File_handling.txt"
 file = open(filename, "r")
-new_list = (file.read().splitlines())
-print(type(new_list))
-print(new_list)
-file.close()
-for i in new_list:
-    print(i)
+lines = file.readlines()
+#lines = (file.readlines()[2])
+print(lines[1])
+print(lines[3])
+
+# Creating a new *.*txt file
+f = open("C:/Users/danie/Documents/סייבר ותכנות/Python/Test123.txt", "x")
+f.close()
+print("\nFile created")
