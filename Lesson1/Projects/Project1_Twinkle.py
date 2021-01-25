fce1429b10b8e5bef1d50daf065fa28a6fb43951
Daniel <danielmp1@gmail.com>
2021-01-25 12:05:23 +0200

    Printing a song on the screen with specific indentation in several lines

---
+++ Lesson1/Projects/Project1_Twinkle.py
@@ -0,0 +1,8 @@
+#The following code will print a song on the screen in a specific way (with indentation in the printed lines)
+
+print('''Twinkle, twinkle, litte star
+        How I wonder what you are!
+                Up above the world so high,
+                Like a diamond in the sky.
+Twinkle, twinkle, little star,
+        How I wonder what you are.''')
