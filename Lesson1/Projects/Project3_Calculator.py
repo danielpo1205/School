897a3abf443de66250a405200e80ea3a7600a728
Daniel <danielmp1@gmail.com>
2021-01-26 15:45:38 +0200

    Calculating the sum of N+NN+NN

---
+++ Lesson1/Projects/Project3_Calculator.py
@@ -0,0 +1,12 @@
+# Reading an integer (n) and then computing it - n+nn+nnn
+
+from time import sleep
+
+N = int(input("Please enter a number: \n"))
+N1 = int("%s" % N)
+N2 = int("%s%s" % (N,N))
+N3 = int("%s%s%s" % (N,N,N))
+sleep(2)
+print("Working....")
+sleep(2)
+print("The result is: ", int(N1+N2+N3))
