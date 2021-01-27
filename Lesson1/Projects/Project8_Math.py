b1e168b53789d46b80e729656e364947b67b9bd4
Daniel <danielmp1@gmail.com>
2021-01-27 18:55:17 +0200

    Solving a simple math problem: (x+y) * (x+y)

---
+++ Lesson1/Projects/Project8_Math.py
@@ -0,0 +1,11 @@
+# Solving a simple math problem: (x+y) * (x+y)
+
+from time import sleep
+
+print("Welcome. lets work the math....")
+sleep(1)
+X = int(input("Please enter the first integer: "))
+Y = int(input("Please enter the second integer: "))
+SUM = (X + Y)*(X + Y)
+sleep(2)
+print("The result is: ", SUM)
