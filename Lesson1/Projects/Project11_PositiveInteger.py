19b7036a2c04cbe1d300dd66d105405007232802
Daniel <danielmp1@gmail.com>
2021-01-31 13:25:24 +0200

    A function that takes a positive integer and returns the sum of the cube of all the smaller positive integers

--- Lesson1/Projects/Project11_PositiveInteger.py
+++ Lesson1/Projects/Project11_PositiveInteger.py
@@ -1,12 +1,23 @@
 # A function that takes a positive integer and returns the sum of the cube of all the smaller positive integers
 
 def cube(n):
-    n -= 1
+    n = n-1
     total = 0
     while n > 0:
-        total += n * n * n
-        n -= 1
-        return total
+        total = total + (n * n * n)
+        n = n-1
+    return total
 
-print("Sum of cubes: ", cube(4))
+print("Sum of cubes: ", cube(5))
 print("Bye bye")
+
+
+# # Another option to solve this:
+# def positive(num):
+#     sum=0
+#     for i in range(1,num):
+#         sum=sum+(i**3)
+#     print("The summary is: ", str(sum))
+#     return sum
+#
+# new_sum=positive(int(input("Enter a positive number: ")))
